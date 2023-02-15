37ad21b4a09f9fe8960f810588c1e87d77efa0f0
r8bywork <vanya.smolensky@gmail.com>
2023-02-15 17:43:52 +0300

    feat: add circle to card

--- src/components/Homepage/Category/card/index.jsx
+++ src/components/Homepage/Category/card/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import style from "./Card.module.scss";
 import Test from "../../.../../../../asssets/images/somestuff/card/test.png";
+import {ReactComponent as Arrow} from "../../.../../../../asssets/images/somestuff/mini/array_right.svg";
 const Card = () => {
   return (
     <div className={style.Card}>
@@ -8,6 +9,9 @@ const Card = () => {
       <div className={style.text}>
         <span className={style.MainText}>Ревизионные люки</span>
         <span className={style.SecText}>на пол, стену, потолок</span>
+      </div>
+      <div className={style.circle}>
+        <Arrow/>
       </div>
     </div>
   );
